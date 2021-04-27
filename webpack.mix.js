8a4fa1682f938ae22f7b04c091afdb5cefe9b8a9
hakan çakmak <hakan.cakmak@live.com>
2021-04-27 12:59:59 +0300

    the project was created with jet. npm and bootstrap are included. template has been integrated. the first mix was applied. The controller has been created. route given.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 const mix = require('laravel-mix');
+const $AdminResAs = 'resources/assets/adminn';
+const $AdminPubAs = 'public/adminn';
 
 /*
  |--------------------------------------------------------------------------
@@ -15,7 +17,30 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
-    ]);
+    ])
+
+.combine([
+    $AdminResAs+'/vendors/perfect-scrollbar/perfect-scrollbar.min.js',
+    $AdminResAs+'/js/bootstrap.bundle.min.js',
+    $AdminResAs+'/vendors/apexcharts/apexcharts.js',
+    $AdminResAs+'/js/pages/dashboard.js',
+    $AdminResAs+'/js/main.js',
+], $AdminPubAs+'/js/maze-admin.js')
+
+.styles([
+    $AdminResAs+'/css/bootstrap.css',
+    $AdminResAs+'/vendors/iconly/bold.css',
+    $AdminResAs+'/vendors/perfect-scrollbar/perfect-scrollbar.css',
+    $AdminResAs+'/vendors/bootstrap-icons/bootstrap-icons.css',
+    $AdminResAs+'/css/app.css',
+], $AdminPubAs+'/css/maze-admin.css')
+
+.sourceMaps();
+mix.version()
+    .browserSync('http://localhost:8000');
+
+
+
 
 if (mix.inProduction()) {
     mix.version();
